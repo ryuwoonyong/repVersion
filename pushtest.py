0fb44d8aca3bd913ee7bb2490a81897fc0830d91
jh <wlsgus9305@gmail.com>
2024-08-05 11:13:33 +0900

    커밋테스트

---
+++ pushtest.py
@@ -0,0 +1,11 @@
+import os, sys, paramiko, subprocess, logging, time
+from os import environ
+
+from util import ReportCommon
+from util import ServerCommon
+
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+from PyQt5.QtCore import QThread, pyqtSignal, QTimer
+
+print("sdfasdfsdf")
